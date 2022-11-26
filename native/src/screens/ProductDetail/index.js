740e97588aa967f3b85160fa9b1c8eaaa8c27486
AmandaCasadem <amanda12kcb@gmail.com>
2022-11-25 23:25:04 -0300

    detalhes finalizado

--- native/src/screens/ProductDetail/index.js
+++ native/src/screens/ProductDetail/index.js
@@ -1,43 +1,42 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { Image, Text, View } from "react-native";
 import { Header } from "../../components/Header/index.js";
+import { ItemDetail } from "../../components/ItemDetail/index.js";
+import { MainContainer } from "../../components/MainContainer/styles.js";
 import { Api } from "../../services";
 
 export const ProductDetail = () => {
-  const navigation = useNavigation();
   const routes = useRoute();
-  const [product, setProduct] = useState();
-
+  const navigation = useNavigation();
+  const [produto, setProduto] = useState([]);
+  
+  // const renderItem = ({item}) => (
+  //   <ItemDetail id={item.id} name={item.nome} photo={item.foto}  description={item.descricao} price={item.preco} qtyStorage={item.qtdEstoque} category={item.categoriaId}/>
+  // );
+  
   useEffect(() => {
-    Api.get(`/produto/${routes.params.id}`).then((response) => {
-      setProduct(response.data);
-    });
+    getProduct();
   }, []);
-
+  
+  const getProduct = async () => {
+    const { data } = await Api.get(`/produto/${routes.params.id}`);
+    setProduto(data);
+    
+  };
+    
   function goBack() {
     navigation.goBack();
-  }
-
+  };
   return (
-    <View>
-      <Header title={"Produtos"} iconName={"arrow-back"} goBack={goBack} />
-      <View
-        style={{
-          flex: 1,
-          paddingLeft: 20,
-          paddingRight: 20,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={{ uri: product?.foto }}
-          style={{ width: 350, height: 350 }}
-        />
-        <Text>{product?.nome}</Text>
-        <Text>R$ {product?.preco.toFixed(2)}</Text>
-      </View>
-    </View>
+    <MainContainer>
+      <Header title={"Detalhe"} iconName={"arrow-back"} goBack={goBack} /> 
+        {/* <FlatList
+        data={produto}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      /> */}
+      <ItemDetail id={produto.id} name={produto.nome} photo={produto.foto}  description={produto.descricao} price={produto.preco} qtyStorage={produto.qtdEstoque} category={produto.categoriaId}/>
+    </MainContainer>
   );
+
 };
